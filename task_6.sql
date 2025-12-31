17fe68f51bbaca5c603fce76292dc1ea180754eb
Tusnelde Endjala <tusneldelainae@gmail.com>
2025-12-31 03:50:33 +0200

    Replace customer records in task_6.sql
    Updated customer records in task_6.sql with new entries.

--- task_6.sql
+++ task_6.sql
@@ -1,12 +1,6 @@
--- task_6.sql
--- Task 6: Populating Your Database with Your More Data
-
--- Ensure we are using the correct database
-USE alx_book_store;
-
--- Insert the multiple customer records using a single INSERT statement
-INSERT INTO Customers (customer_id, customer_name, email, address)
+-- Insert multiple rows into the 'customer' table
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+    (2, 'Kamau Musau', 'kmusau@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Clara Michael', 'cmichael@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Layla Juma', 'ljuma@sandtech.com', '126 Happiness  Ave.');
